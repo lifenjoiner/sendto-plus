039302f36fc5795a4a09d237af6026431e6daee0
YX Hao <lifenjoiner@163.com>
2017-03-23 23:46:05 +0800

    Auto append arch sufix when build

--- make-vc.bat
+++ make-vc.bat
@@ -1,6 +1,16 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 pushd %~dp0
 
-cl.exe /MD /Os /DUNICODE /D_UNICODE sendto+.c Ole32.lib shell32.lib user32.lib Comdlg32.lib Shlwapi.lib
+set appname=sendto+
+set arch=
+for /f "usebackq delims=" %%i in (`"cl.exe 2>&1"`) do (
+    if "!arch!"=="" (
+        set arch=%%i
+        set arch=!arch:~-2!
+    )
+)
+
+cl.exe /MD /Os /DUNICODE /D_UNICODE /Fe%appname%_x%arch%.exe %appname%.c Ole32.lib shell32.lib user32.lib Comdlg32.lib Shlwapi.lib
 
 popd
